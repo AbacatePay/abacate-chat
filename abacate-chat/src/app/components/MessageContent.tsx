22ba97e34bc18cf04f0999c86942ebd96359868e
Luiz Gustavo <62274995+luizgcl@users.noreply.github.com>
2025-07-03 11:49:47 -0300

    feat: create copy button in code blocks

--- abacate-chat/src/app/components/MessageContent.tsx
+++ abacate-chat/src/app/components/MessageContent.tsx
@@ -1,11 +1,39 @@
-import { Loader2 } from "lucide-react";
+import { useMarkdown } from "@/app/hooks/use-markdown";
+import { Check, Copy, Loader2 } from "lucide-react";
+import { useState } from "react";
 import ReactMarkdown from "react-markdown";
 import remarkGfm from "remark-gfm";
-import { useMarkdown } from "@/app/hooks/use-markdown";
 
 interface MessageContentProps {
   content: string | null;
 }
+
+const CopyablePre = (props: any) => {
+  const [isCopied, setIsCopied] = useState(false);
+
+   const handleCopy = async () => {
+    try {
+      await navigator.clipboard.writeText(props.children.props.children);
+      setIsCopied(true);
+      setTimeout(() => setIsCopied(false), 2000);
+    } catch (err) {
+      console.error("Failed to copy text: ", err);
+    }
+  };
+
+  return (
+    <div style={{ position: "relative" }}>
+      <pre {...props} />
+      <button
+        onClick={handleCopy}
+        className="text-sm z-10 absolute top-2 right-2 rounded-sm flex items-center justify-center gap-2 px-2 py-1 hover:cursor-pointer bg-white hover:bg-gray-100"
+      >
+        {isCopied ? <Check /> : <Copy />}
+        <span>{isCopied ? 'Copiado' : 'Copiar'}</span>
+      </button>
+    </div>
+  );
+};
 
 export const MessageContent = ({ content }: MessageContentProps) => {
   const { components } = useMarkdown();
@@ -16,7 +44,10 @@ export const MessageContent = ({ content }: MessageContentProps) => {
 
   return (
     <div className="text-sm font-normal leading-relaxed break-words overflow-hidden">
-      <ReactMarkdown remarkPlugins={[remarkGfm]} components={components}>
+      <ReactMarkdown remarkPlugins={[remarkGfm]} components={{
+        ...components,
+        pre: CopyablePre,
+      }}>
         {content}
       </ReactMarkdown>
     </div>
