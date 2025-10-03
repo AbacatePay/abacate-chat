ece1218934ef879d0fe8bc2e56c7134321badad8
Daniel Limae <danielunivap@outlook.com>
2025-10-03 17:03:51 -0300

    feat: command from shadcn

--- abacate-chat/src/app/components/search-academy.tsx
+++ abacate-chat/src/app/components/search-academy.tsx
@@ -1,8 +1,33 @@
-export default function SearchAcademy() {
+import {
+  Command,
+  CommandInput,
+  CommandList,
+  CommandEmpty,
+  CommandGroup,
+  CommandItem,
+  CommandSeparator,
+} from "./ui/command";
 
+export default function SearchAcademy() {
   return (
     <div>
-
+      <Command>
+        <CommandInput placeholder="Type a command or search..." />
+        <CommandList>
+          <CommandEmpty>No results found.</CommandEmpty>
+          <CommandGroup heading="Suggestions">
+            <CommandItem>Calendar</CommandItem>
+            <CommandItem>Search Emoji</CommandItem>
+            <CommandItem>Calculator</CommandItem>
+          </CommandGroup>
+          <CommandSeparator />
+          <CommandGroup heading="Settings">
+            <CommandItem>Profile</CommandItem>
+            <CommandItem>Billing</CommandItem>
+            <CommandItem>Settings</CommandItem>
+          </CommandGroup>
+        </CommandList>
+      </Command>
     </div>
   );
 }
