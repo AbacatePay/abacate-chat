cbe0b7cf77c604e4d1bf1fb0ace64bfac948b3f6
Vinicius Rocha <work@hayato.me>
2025-10-04 17:35:29 -0300

    feat: implement SegmentedControl component with dynamic tab management and styling for improved user interaction

---
+++ abacate-chat/src/app/components/ui/segmented-control.tsx
@@ -0,0 +1,101 @@
+"use client";
+
+import * as React from "react";
+import { Slottable } from "@radix-ui/react-slot";
+import * as TabsPrimitive from "@radix-ui/react-tabs";
+import mergeRefs from "merge-refs";
+
+import { useTabObserver } from "@/app/hooks/use-tab-observer";
+import { cn } from "@/app/components/lib/utils";
+
+const SegmentedControlRoot = TabsPrimitive.Root;
+SegmentedControlRoot.displayName = "SegmentedControlRoot";
+
+const SegmentedControlList = React.forwardRef<
+  React.ComponentRef<typeof TabsPrimitive.List>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.List> & {
+    floatingBgClassName?: string;
+  }
+>(({ children, className, floatingBgClassName, ...rest }, forwardedRef) => {
+  const [lineStyle, setLineStyle] = React.useState({ width: 0, left: 0 });
+
+  const { mounted, listRef } = useTabObserver({
+    onActiveTabChange: (_, activeTab) => {
+      const { offsetWidth: width, offsetLeft: left } = activeTab;
+      setLineStyle({ width, left });
+    },
+  });
+
+  return (
+    <TabsPrimitive.List
+      ref={mergeRefs(forwardedRef, listRef)}
+      className={cn(
+        "relative isolate grid w-full auto-cols-fr grid-flow-col gap-1 rounded-full bg-weak-50 p-1 border border-border",
+        className
+      )}
+      {...rest}
+    >
+      <Slottable>{children}</Slottable>
+
+      {/* floating bg */}
+      <div
+        className={cn(
+          "absolute inset-y-1 left-0 -z-10 rounded-full bg-background shadow-sm border transition-transform duration-300",
+          {
+            hidden: !mounted,
+          },
+          floatingBgClassName
+        )}
+        style={{
+          transform: `translate3d(${lineStyle.left}px, 0, 0)`,
+          width: `${lineStyle.width}px`,
+          transitionTimingFunction: "cubic-bezier(0.65, 0, 0.35, 1)",
+        }}
+        aria-hidden="true"
+      />
+    </TabsPrimitive.List>
+  );
+});
+SegmentedControlList.displayName = "SegmentedControlList";
+
+const SegmentedControlTrigger = React.forwardRef<
+  React.ComponentRef<typeof TabsPrimitive.Trigger>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Trigger>
+>(({ className, ...rest }, forwardedRef) => {
+  return (
+    <TabsPrimitive.Trigger
+      ref={forwardedRef}
+      className={cn(
+        // base
+        "peer",
+        "relative z-10 h-8 whitespace-nowrap rounded-full px-4 py-2 text-sm font-semibold outline-none",
+        "flex items-center justify-center gap-2",
+        "transition-colors duration-200",
+        // focus
+        "focus:outline-none",
+        // active
+        "data-[state=active]:text-foreground",
+        // inactive
+        "data-[state=inactive]:text-soft-400",
+        className
+      )}
+      {...rest}
+    />
+  );
+});
+SegmentedControlTrigger.displayName = "SegmentedControlTrigger";
+
+const SegmentedControlContent = React.forwardRef<
+  React.ComponentRef<typeof TabsPrimitive.Content>,
+  React.ComponentPropsWithoutRef<typeof TabsPrimitive.Content>
+>(({ ...rest }, forwardedRef) => {
+  return <TabsPrimitive.Content ref={forwardedRef} {...rest} />;
+});
+SegmentedControlContent.displayName = "SegmentedControlContent";
+
+export {
+  SegmentedControlRoot as Root,
+  SegmentedControlList as List,
+  SegmentedControlTrigger as Trigger,
+  SegmentedControlContent as Content,
+};
